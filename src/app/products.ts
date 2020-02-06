9c2b228f1b3d8dde280bf6d389dba41ef8df0f77
itshpit <itshpit@gmail.com>
2020-02-06 09:15:36 +0200

    3 files modified, 2 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,27 @@
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
+    name: 'Samsung',
+    number: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
-    price: 699,
+    name: 'Apple',
+    number: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
-    price: 299,
+    name: 'Huawei',
+    number: 299,
+    description: ''
+  },
+  {
+    name: 'Google',
+    number: 299,
+    description: ''
+  },
+  {
+    name: 'OnePlus',
+    number: 299,
     description: ''
   }
 ];
